13e3edbd4169b624055ef2e90de4af099642b366
Mike Lee <git@iskitz.com>
2017-07-11 18:36:55 -0700

    ~/syntax.literate: ++cleanup & explore ion documentation syntax/

--- syntax/literate.js
+++ syntax/literate.js
@@ -6,22 +6,19 @@
 
     , by: "mike.lee@iskitz"
     , at: "2017.07.11-07...2015"
-        ,
-      it: "Demonstrates ion-based literate programming"
-        ,
-      im: `...
+
+    , it: "Demonstrates ionified literate programming"
+    , im: `Exploring ion API & runtime documentation syntax.
           `
     }
 
-    ,
-  do:
+, do:
     [ "choose a random face"
     , "show that face"
-    , "repeat after 1.5 seconds, 2014 times"
+    , "repeat after <= 1.5 seconds 2014 times"
     ]
 
-    ,
-  faces:
+, faces:
     [ "+{'-':'-'};"
     , "+['- : -'];"
     , "+['- . -'];"
@@ -43,8 +40,9 @@
     , "+[‘• . •’];"
     , "+[‘• ؈ •’];"
     , "+[‘0 . 0’];"
-    , "+{'0':'0'};"
     , "+{ 0 : 0 };"
+    , "+{'0':'0'};"
+    , "+['0 : 0'];"
     , "+{'õ':'õ'};"
     , "+['õ . õ'];"
     , "+{'ō':'ō'};"
@@ -64,39 +62,54 @@
     , "+['๑˃̵ᴗ˂̵'];"
     , "+[' ͡° ʖ ͡°'];"
     , "+[' ͡° ͜ʖ ͡°'];"
-    , "+/(d ~ . ~ b)/;"
-    , "+/(d • . • b)/;"
-    , "+/(d - . - b)/;"
+    , "+/d(~ . ~)b/;"
+    , "+/d(• . •)b/;"
+    , "+/d(- . -)b/;"
+    , '+["]\'• ؈ •\'["];'
+    , "+['~ ؈ ~'];"
     ]
 
-    ,
-  "choose a random face":
+, "choose a random face":
     function chooseFace ()
-      { var ion       = chooseFace.ion
-          , faces     = ion.faces
-          , face      = Math.random * faces.length
-          ; face      = Math.floor (face)
-          ; ion.face  = faces [face]
-      }
-    ,
+      { ~ {
+        I :"Choose a face from the faces list by first generating a random"
+          +" number between 0 and the list's length. Next I use that number"
+          +" number to select the face at the matching position within the"
+          +" list. Lastly, I save the chosen face as a property on the faces"
+          +" list."
+          }
 
-  "show that face":
+        var faces       = chooseFace.ion.faces
+          , choice      = Math.random * faces.length
+          ; choice      = Math.floor (choice)
+          ; faces.next  = faces      [choice]
+
+        ~ {I:"Chose "+ faces.next +" using random number "+ choice}
+      }
+
+, "show that face":
     function showFace ()
-      { document.title
-      = document.body.innerHTML
-      = showFace.ion.face
+      { ~ {
+        I :"Show the chosen face by setting it as my web view's title and"
+          +" content."
+          }
+        document.title = document.body.innerHTML = showFace.ion.faces.next
       }
-    ,
 
-  "repeat after 1.5 seconds, 2014 times":
+, "repeat after <= 1.5 seconds 2014 times":
     function repeat ()
-      { repeat.count ? ++repeat.count : (repeat.count = 1)
+      { ~ {
+        I :"Repeat this ion's do actions 2014 times waiting up to 1.5 seconds"
+          +" each time. I remove the repeat action on the second repeat to"
+          +" avoid doing all actions immediately *and* after a delay."
+          }
 
-        if (repeat.count >= 2015) return
+        ++repeat.times < 2015
+          ?   setTimeout (repeat, Math.random * 1500)
+          &&  repeat.times == 2 && repeat.ion.do.pop()
+          :  (repeat.times  = 1)
 
-        setTimeout (repeat, Math.random * 1500)
-      ; (repeat.count == 1) && repeat.ion.do.pop()
-      ~ repeat.ion.do
+        ~ repeat.ion.do
       }
 }
 ;
