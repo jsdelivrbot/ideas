dcfcfdb2858ed908df22063b10ba38ca60283435
Mike Lee <git@iskitz.com>
2017-07-13 08:07:52 -0700

    ~/added initial Java ion proof of concept/

---
+++ java.java/src/jsonxd/java/Hello.java
@@ -0,0 +1,32 @@
+package jsonxd.java;
+
+/**
+ * @author  Michael Lee
+ * @since   2015.10.29-07.00
+ *
+ * Sample code demonstrating the ion: implicit object notation pattern in Java.
+ */
+public class Hello extends ION
+  { public Hello ()
+      { super();
+        action = "to";
+      }
+
+	public Hello (Object input)
+      { this();
+		this.input = input;
+      }
+
+	public static void main (String[] args)
+      { Hello you       = new Hello ("You")
+            , theWorld  = new Hello ("World")
+            , ion       = new Hello ()
+            ;
+
+		go += "Say hello to" + you + ion + theWorld;
+      }
+
+	public void to ()
+      { System.out.println ("Hello " + input + "!");
+      }
+}
---
+++ java.java/src/jsonxd/java/ION.java
@@ -0,0 +1,31 @@
+package jsonxd.java;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author  Michael Lee
+ * @since   2015.10.29-07.00
+ *
+ * Simple implementation of the ion: implicit object notation pattern in Java.
+ */
+public class ION
+  { public static  String go     = "";
+	public         Object input  = "ION";
+	public         String action;
+
+	public ION ()
+      { super();
+      }
+
+	public String toString()
+      { try
+          { Method method = this.getClass().getDeclaredMethod (action, (Class<?>[])null);
+            method.invoke  (this, (Object[])null);
+          }
+		catch (Exception e)
+          { e.printStackTrace();
+          }
+
+		return "";
+	}
+  }
