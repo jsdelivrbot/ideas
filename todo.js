436e1c50c8afcbf5d60c97eb3d5060df392809c9
Mike Lee <git@iskitz.com>
2018-01-05 03:01:01 -0800

    ~/todo.js: added to track ideas to do/

---
+++ todo.js
@@ -0,0 +1,38 @@
+;
+~
+{ re:
+    { id: "todo.ideas@ionify"
+    , is: "ionify's list of ideas To Do"
+    , by:["mike.lee@iskitz", "team@ionify"]
+    , at: "2018.01.04-08...2007.09-04"
+
+    , im:
+        [`...
+         `
+         ]
+    }
+
+, todo
+:   [ { "2018.__.__-08"
+      :   { 
+          }
+          
+      , "2018.01.04-08"
+      :   { packages
+          :   `Be like a npm for the web.
+               Sit on top of things like cdn.jsdelivr.net.
+              `
+          , notability
+          :   `Contact Donald E. Knuth about ionify & literate programming.
+              `
+          }
+      }
+    ]
+
+, DONE
+:   { "year[.month.day.hour.minute.second][+-]timezoneHours[.minutesOffset]":
+        [
+        ]
+    }
+}
+;
