6b4cf1b6393ce3a1486dedd86ec03728a2e10336
Mike Lee <git@iskitz.com>
2018-01-15 10:40:51 -0800

    python: added ~ion & -ion tests & linked to numeric type emulation docs

--- python/ion.proof.py
+++ python/ion.proof.py
@@ -1,7 +1,7 @@
 # id: python.proof@ideas.ionify.net
 # by: mike.lee@iskitz
-# at: 2017.07.13-07...2016.12.15-08
-# is: A proof that ion: implicit object notation can be implemented in Python
+# at: 2018.01.13-08...2016.12.15-08
+# is: A proof that ion: implicit object notation can be implemented in Python. For the set of methods needed to fully implement ion in python see: https://docs.python.org/3.6/reference/datamodel.html#emulating-numeric-types
 
 class Ion:
     def getName(self):
@@ -20,7 +20,7 @@ class Ion:
         return '-hipion!'
 
     def __pos__(self):
-        print ('+' + self.getName())#self.name)
+        print ('+' + self.getName())
         return '+hipion!'
 
     def __invert__(self):
@@ -28,7 +28,9 @@ class Ion:
         return '~hipion!'
 
 ion = Ion()
-+ion
+~ ion
++ ion
+- ion
+ion + 777
 repr (ion)
 print (ion)
-ion + 777
