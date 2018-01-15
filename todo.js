d7d43caf19a19e6d9e2331831892bbfdc9aed4cf
Mike Lee <git@iskitz.com>
2018-01-15 10:56:14 -0800

    todo: added terminology & action memory notes

--- todo.js
+++ todo.js
@@ -17,6 +17,17 @@
       :   { 
           }
           
+      , "2018.01.12-08"
+      :   { terminology
+          :   `ionify: implicit object notations invented for you, is an api for expressing ideas via ions
+              .  ions: implicit object notations, are expressions that activate objects via itc
+              .   itc: interactive type conversion, is a design pattern for interacting with a data type's conversion
+              . javascript supports hip: held-in-place & hop: held-on-prototype itc via instance & prototype valueOf() 
+              . java       supports hot: held-on-type itc via class' toString() instance method + Reflection
+              . python     supports hot: held-on-type itc via numeric type emulation: https://docs.python.org/3.6/reference/datamodel.html#emulating-numeric-types
+              `
+          }
+          
       , "2018.01.04-08"
       :   { packages
           :   `Be like a npm for the web.
@@ -26,6 +37,15 @@
           :   `Contact Donald E. Knuth about ionify & literate programming.
               `
           }
+          
+      , "2007"
+      :    { actions
+           :   `Store info about activated ions so that subsequent & prior ions can act
+                on them, e.g. interpreting orion sentences: they - began - to - fly
+                they could look at past actions and tag itself with possible candidates.
+               `
+               
+           }
       }
     ]
 
