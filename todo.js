569035270151b0cba4cee3b5aef94de64fc56cc3
Mike Lee <git@iskitz.com>
2018-05-02 06:11:53 -0700

    todo: added aesop & expanded itc descriptions & tidied most "s to 's

--- todo.js
+++ todo.js
@@ -1,10 +1,10 @@
 ;
 ~
 { re:
-    { id: "todo.ideas@ionify"
+    { id: 'todo.ideas@ionify'
     , is: "ionify's list of ideas To Do"
-    , by:["mike.lee@iskitz", "team@ionify"]
-    , at: "2018.01.04-08...2007.09-04"
+    , by:['mike.lee@iskitz', 'team@ionify']
+    , at: '2018.04.20-07...2007.09-04'
 
     , im:
         [`...
@@ -13,22 +13,50 @@
     }
 
 , todo
-:   [ { "2018.__.__-08"
-      :   { 
+:   [ { '20__.__.__-0_'
+      :   {
           }
-          
-      , "2018.01.12-08"
+
+      , '2018.04.20-07'
+      :   { aesop
+          :   `match actions by which is most inclusive of others, i.e.
+               match ~get,in,then for:
+                   get ion, aeon, and do in order then do web and get ions
+               rather than:
+                   do in order
+                   do web
+                   get ions
+                   get ion, aeon, and do
+               because each of those is a ~get,in,then value
+              `
+          }
+
+      , '2018.01.12-08'
       :   { terminology
           :   `ionify: implicit object notations invented for you, is an api for expressing ideas via ions
               .  ions: implicit object notations, are expressions that activate objects via itc
-              .   itc: interactive type conversion, is a design pattern for interacting with a data type's conversion
-              . javascript supports hip: held-in-place & hop: held-on-prototype itc via instance & prototype valueOf() 
-              . java       supports hot: held-on-type itc via class' toString() instance method + Reflection
-              . python     supports hot: held-on-type itc via numeric type emulation: https://docs.python.org/3.6/reference/datamodel.html#emulating-numeric-types
+              .   itc: interactive type conversion, is a design pattern for interacting with a data type's
+                       conversion|comparison:co---r--on
+                       convarison,convarsion,comversion,comverison,comvarison? :-)
+
+              . javascript supports hip: held-in-place & hop: held-on-prototype itc via instance & prototype
+                            .valueOf() methods
+
+              . java       supports hot: held-on-type itc via objects' .toString() method [+ Reflection]
+                            enables pre-Garbage-Collection notification via objects' .finalize() method
+
+              . python     supports hot: held-on-type itc via numeric type emulation:
+                            https://docs.python.org/3.6/reference/datamodel.html#emulating-numeric-types
+                            enables sensing which [punctuation] operators were applied! <-- my js wishlist
+
+              . others     support  goo: generic operator overloading itc:
+                            operator overloading without needing to define as a type's member function
+                            https://en.wikipedia.org/wiki/Operator_overloading#Catalog
+                            https://stackoverflow.com/questions/4421706/what-are-the-basic-rules-and-idioms-for-operator-overloading#4421719
               `
           }
-          
-      , "2018.01.04-08"
+
+      , '2018.01.04-08'
       :   { packages
           :   `Be like a npm for the web.
                Sit on top of things like cdn.jsdelivr.net.
@@ -37,20 +65,20 @@
           :   `Contact Donald E. Knuth about ionify & literate programming.
               `
           }
-          
-      , "2007"
+
+      , '2007'
       :    { actions
            :   `Store info about activated ions so that subsequent & prior ions can act
                 on them, e.g. interpreting orion sentences: they - began - to - fly
                 they could look at past actions and tag itself with possible candidates.
                `
-               
+
            }
       }
     ]
 
 , DONE
-:   { "year[.month.day.hour.minute.second][+-]timezoneHours[.minutesOffset]":
+:   { 'year[.month.day.hour.minute.second][+-]timezoneHours[.minutesOffset]':
         [
         ]
     }
