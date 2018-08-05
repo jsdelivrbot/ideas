80432ada615d143194a1cce614f0c5354f09d1d6
Mike Lee <git@iskitz.com>
2018-08-05 01:56:51 -0700

    todos: added terminology & implementation proposals

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@
     { id: 'todo.ideas@ionify'
     , is: "ionify's list of ideas To Do"
     , by:['mike.lee@iskitz', 'team@ionify']
-    , at: '2018.05.19-07...2007.09-04'
+    , at: '2018.08.04-07...2007.09-04'
     , im:
         [`...
          `
@@ -17,15 +17,276 @@
       :   {
           }
 
-      , '2018.05.11-07'
+      , '2018.08.03-07'
+      :   { lingo
+          :   'Create a readme section that lists lingo like itc & itcandoo'
+          
+          , terms
+          :   { ion     : 'invoked object notation,'
+                        + ' enables writing independently observable & notifiable'
+                        + ' data and code.'
+                        
+              , support : 'ion can be implemented via interactive type conversion'
+                        + ' and operator overloading in supporting languages.'
+                        + " It's supported by hip-hop & hot itc in js & java"
+                        + ' respectively, and operator overloading in python to'
+                        + ' name a few.'
+                        
+              , itcandoo: 'interactive type conversion and operator overloading'
+              , itc     : 'interactive type conversion is interacting with one or more types during their conversion'
+              
+              , ionify  : 'ion implemented for you, is an api for ion interaction'
+                        + ', observation & notification'
+                        
+              , hip     : 'handled in-place'
+              , hop     : 'handled on prototype'
+              , hot     : 'handled on type'
+              , lion    : 'literal          ion'
+              , rion    : 'referenced       ion'
+              , orion   : 'object reference ion'
+              , jedi    : 'json-expressed   ion'
+              , oj      : 'observable      json'
+              , im      : 'ion module'
+              
+              , ai      : 'anonymous        ion'
+              , ai      : 'accessible       ion'
+              , li      : 'literal          ion'
+              , ni      : 'named            ion'
+              , ri      : 'referrable       ion'
+              , nion    : 'named            ion'
+              , union   : 'unnamed          ion'
+              , anion   : 'anonymous        ion'
+              
+              , ionify  : 'ion interpreted for you'
+              , ionify  : 'ion interfaces  for you'
+              , hip     : 'held in-place'
+              , hop     : 'held on prototype'
+              , hot     : 'held on type'
+              , foo     : 'functional operator overloading'
+              , goo     : 'generic    operator overloading'
+              }
+          
+          , loops
+          :   ~
+              { redo :'step'
+              , until: 2055 || 761 || 2055
+              
+              , step
+              :   function step ()
+                    { console.log ('redos: '+this.until)
+                    }
+                    
+              , valueOf
+              :   function ionify ()
+                    {// ionify.ion = this
+                    ;/*if ( this .until--)
+                         { this [this.redo] ()
+                         ~ this
+                         }/*/
+                    ;   this .until--
+                    &&  this [this.redo] ()
+                    &   this/* || 0
+                    &!  function(){~ionify.ion}()*/
+                    } 
+              }
+          }
+
+      , '2018.07.09-07'
+      :   { ionify
+          :   [ ""
+              , "dsl for easier user adoption"
+              , "call-by-meaning programming"
+              ]
+          }
+
+      , '2018.06.20-07'
+      :   { ionify
+          :   { sense
+              :   { sensors :''
+                  , ions    :''
+                  , actions :''
+                  }
+              , host
+              :   { get :'fetch'
+                  , put :'save'
+                  , show:'render'
+                  , find:'locate ions'
+                  , vet :'authenticate'
+                  }
+              , sensor
+              :   { sense:'shape,content'
+                  , scope:'shared space'
+                  , acts :'actions'
+                  }
+              }
+          , __:
+              `ionify: invoked object notation implemented for you
+              .Each type sensor defined as own ion
+                  ion   : ~{} invoked object notation used to sense all other ions
+                  aeon  : ~[] array-expressed ion
+                  stori : ~// string template or regular-expression ion ~//
+              `
+                  + {id:'ion'  , on:Object, do:Function||/ /||[]}
+                  + {id:'aeon' , on:Array , do:Function||/ /||{}}
+                  + {id:'stori', on:RegExp, do:Function|| {}||[]}
+                  +
+              `sense@ionify defines what's common among all ions
+              
+                           id: uniquely identifies ions
+                        do_on: actions to do
+                      dont_on: actions to 
+                  this|ion|id: ion member's reference to their containing ion
+                  
+                  ? how to support synonyms like .this, .ion & .id
+                  
+              .host@ionify are ionify's environment starting points
+              `
+          }
+
+      , '2018.06.19-07'
+      :   { function
+          :   { sensing
+              :   `Enable ajile's recall (past-sensing) via
+              
+                   0: ion-recall requests
+                  `
+                      +
+                      { re:
+                          {   id: 'an.ion'
+                          , call: Boolean || 'true:default, false:opt-out explicitly'
+                          }
+                      }
+                      +
+                  `
+                   1: remembering ion by reference (runtime) then structure (persistent storage)
+                  `
+                      +
+                      { 'ion.path'         : ['ion.id.unique', 'ion.id.unique.other']
+                      , 'ion.path.2'       : ['ion.id.unique', 'ion.id.etc'  , '...']
+                      , 'ion.path:type.etc': ['ion.id.unique', 'ion.id.unique.other']
+                      }
+                      +
+                      { 'ion.id.unique'      : Object || 'reference to an object'
+                      , 'ion.id.unique.other': String || 'ion message or uri'
+                      }
+                      +
+                  ` 
+                   2: adding ion's recall sensor 
+                  `
+              }
+          }
+
+      , '2018.06.18-07'
+      :   { syntax:
+              { do:
+                  `Make ~do actions always the 1st sensed since all ions "do" something
+                   even if only self-announcing.
+                   This enables simpler definition of multiple ~on actions since they can
+                   now be a list of individual ~ons.
+                  `
+                  + { do:
+                        [ {on : 'ion.member.name   as   string   or    number'}
+                        , {on : 'ion.member.name', as: 'string', or : 'number'}
+                        , {on : 'ion.member.name', is: 'value' , or : /value/ }
+                        , {on : 'ion.member.name   is   value    or   /value/'}
+                        , {on : 'ion/member/path'              }
+                        , {on : ['ion', 'member', 'names', '&', 'paths']}
+                        ]
+                    }
+                  + "Or maybe use ~on:[] lists and keep ~do's priority as is."
+              }
+          }
+
+      , '2018.06.03-07...2018.06.18-07'
+      :   { todos
+          :   { will
+              :   [ {queries : 'need lookups'}
+                  , {  get   : 'need fetching'}
+                  , { server : 'want authentication'}
+                  , {modules : 'want encapsulation'}
+                  , { coding : 'want simpler coding'}
+                  ]
+              , need
+              :   { queries
+                  :   "Query language for identifying object parts, e.g.:"
+                      + { on : 'id@host' }
+                      | { on : 'field'
+                        , as : 'String'
+                        , is : 'value'
+                        , in : 'ion'
+                        }
+                      | { on : 'field as string is "value" in ion'}
+
+                  +   `Build natural language queries atop vocab'd ions; see ~do
+                       syntax notes from 2018.06.18-07
+                      `
+                  }
+              , want
+              :   { modules
+                  :  `Modularize all of ionify's parts`
+                  , authentication
+                  :  `Confirm ions with trusted authority (ionified.io,ionified.net,glitch) or specified domain`
+                  , programming_language
+                  :  `Define own programming constructs, e.g. repeat<=for,while,do & array-like sets`
+                  , documentation_api
+                  :  `Auto generate runtime api docs from ~re's`
+                  , documentation
+                  :  `Rewrite to focus on specific audiences
+                       ion   : how = developers
+                             : why = creators
+                             : app = all
+                       ionify: how = developers
+                             : why = creators
+                             : app = developers
+                     `
+                  , server
+                  :  `Use glitch or repl.it as node hosts that do ion
+                     :  authentication
+                     :  location resolution
+                     :  assessment
+                     :  substitution
+                     !YES ¿Can private glitches be referenced publicly
+                     `
+                  , schema
+                  :  `¿Use json-schema`
+                  , across_program_languages
+                  :  `Implement in other programming languages`
+                  }
+              }
+          }
+
+      , '2018.05.11-07...2018.07.07-07'
       :   { terminology
-          :   { itcandoo
-              :   'interactive type conversion'
-              +   'and'
-              +   'operator overloading'
+          :   { ion
+              :   "invoked object notation is a software language syntax for"
+              +   " expressing data & code as independent, observable &"
+              +   " notifiable objects"
+              
+              |   "invoked object notation is a software language syntax for"
+              +   " expressing data & code as indirectly observable &"
+              +   " notifiable objects"
+              
+              , ionify
+              :   "ions interpreted for you is an api for interpreting "
+              +   " invoked object notations"
+              
+              , itcandoo
+              :   {'itc: interactive type conversion'
+                  :   "is the software language capability that enables"
+                  +   " interacting with a dataype during its conversion to"
+                  +   " another type"
+                  , and
+                  :   ""
+                  ,'oo: operator overloading'
+                  :   "is the software language capability that enables"
+                  +   " defining how an operator interacts with one or more"
+                  +   " datatypes"
+                  }
+              
               , application
-              :   "ion & ionify can be implemented in itcandoo languages"
-              +   "that is, languages that support interactive type conversion and operator overloading"
+              :   "ions can be used & ionify can be implemented in, itcandoo"
+              +   " software languages, that is, languages that support"
+              +   " interactive type conversion and operator overloading"
               }
           }
       , '2018.05.06-07'
