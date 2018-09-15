0a64b1f15f47b7a3f7f9f464114ebb3625f2717f
Mike Lee <git@iskitz.com>
2018-09-15 12:09:03 -0700

    java: ++code.readability
    via consistent new lines, indentation, and operator & punctuation
    placement.

--- java/src/net/ionify/java/Hello.java
+++ java/src/net/ionify/java/Hello.java
@@ -2,33 +2,36 @@ package net.ionify.java;
 
 /**
  * @author  Mike.Lee@iskitz
- * @since   2018.04.26-07...2015.10.29-07.00
+ * @since   2018.05.30-07...2015.10.29-07.00
  *
  * Sample code demonstrating the ion: implicit object notation pattern in Java.
  */
 public class Hello extends ION
-  { public Hello ()
-      { super();
-        action = "to";
-      }
+  { public
+      Hello ()
+        { super ()     ;
+          action = "to";
+        }
 
-	public Hello (Object input)
-      { this();
-		this.input = input;
-      }
+    public
+      Hello (Object input)
+        { this ()           ;
+          this.input = input;
+        }
 
-	public static void main (String[] args)
-      { Hello you       = new Hello ("You")
-            , theWorld  = new Hello ("World")
-            , ion       = new Hello ()
-            ;
+    public static void
+      main (String[] args)
+        { Hello
+            you       = new Hello ( "You" ),
+            theWorld  = new Hello ("World"),
+            ion       = new Hello (       );
 
-		go += "Say hello to" + you + ion + theWorld;
+          go += "Say hello to" + you + ion + theWorld;
+          System.out.println (go)                    ;
+        }
 
-        System.out.println (go);
-      }
-
-	public void to ()
-      { System.out.println ("Hello " + input + "!");
-      }
+    public void
+      to ()
+        { System.out.println ("Hello " + input + "!");
+        }
 }
--- java/src/net/ionify/java/ION.java
+++ java/src/net/ionify/java/ION.java
@@ -4,28 +4,30 @@ import java.lang.reflect.Method;
 
 /**
  * @author  Mike.Lee@iskitz
- * @since   2018.04.26-07...2015.10.29-07.00
+ * @since   2018.05.30-07...2015.10.29-07.00
  *
  * Simple implementation of the ion: implicit object notation pattern in Java.
  */
 public class ION
-  { public static  String go     = "";
-	public         Object input  = "ION";
-	public         String action;
+  { public static  String go     = ""   ;
+    public         Object input  = "ION";
+    public         String action        ;
 
-	public ION ()
-      { super();
-      }
+    public
+      ION ()
+        { super ();
+        }
 
-	public String toString()
-      { try
-          { Method method = this.getClass().getDeclaredMethod (action, (Class<?>[])null);
-            method.invoke  (this, (Object[])null);
-          }
-		catch (Exception e)
-          { e.printStackTrace();
-          }
-
+    public String
+      toString ()
+        { try
+            { Method method = this.getClass()
+                                  .getDeclaredMethod (action, (Class<?>[]) null);
+              method.invoke  (this, (Object[]) null)                            ;
+            }
+          catch (Exception e)
+            { e.printStackTrace ();
+            }
 		return " " + input;
-	}
+        }
   }
