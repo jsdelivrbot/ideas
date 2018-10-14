b05895d7b38467f543dfbed19d815131869628ce
Mike Lee <git@iskitz.com>
2018-10-15 05:55:52 +0900

    syntax: string ion: tidied & ++invocation.attempts

--- syntax/ion.via.string.valueof.js
+++ syntax/ion.via.string.valueof.js
@@ -6,7 +6,7 @@
     , is: "an exploration of ion via strings that activate js' "
         + "toString & valueOf abstract operations."
     , by: 'mikelee@ionify.net'
-    , at: '2018.10.13+09...2007.09-04'
+    , at: '2018.10.15+09...2007.09-04'
     }
     ,
   valueOf ()
@@ -17,21 +17,21 @@
     }
     ,
   onString ()
-    { console.log (this)
+    { console.log (Array.prototype.join.call (this, ''))
     }
     ,
   try ()
-    { new String ('hi!')
-    | "did this work?" [0.0] + "i"
-    ~ "¿what about this" | "this?"
+    { new String ('no')
+    - "did this work?" [0.0] + "no"
+    ~ "¿what about this" | "this?" | "no"
     ~ []["test"] + {}["hi"]
     ~  "example"  -  "punctuation"
-    ^   "aesop"   &  "  stories  "  >>  {are:"ions"}
-    *   "these"   +  "expressions"  <<  {are:"too" }
-    /   "write"   %  "them as you"  |   {see:"fit" }
-    >   "using"  >=  "  any of   "  <=     "these" 
+    ^   "aesop"   &  "  stories  "  >>  "are ions"
+    *   "these"   +  "expressions"  <<  "are too"
+    /   "write"   %  "them as you"  |   "see fit"
+    >   "using"  >=  "  any of   "  <=   "these" 
     <     17     ==  " compatible"  !=  "operators!"
-    ;
+    ; "¿ this worked ?" ^ new String ("no")
     }
 }
 
